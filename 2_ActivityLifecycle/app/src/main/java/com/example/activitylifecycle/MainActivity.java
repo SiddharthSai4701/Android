6c5e59094094bb6472b36f36fe412ecceaac20f8
Siddharth Vijay Sai <67595252+SiddharthSai4701@users.noreply.github.com>
2023-11-04 01:15:37 +0530

    Functional app

--- 2_ActivityLifecycle/app/src/main/java/com/example/activitylifecycle/MainActivity.java
+++ 2_ActivityLifecycle/app/src/main/java/com/example/activitylifecycle/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.activitylifecycle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,71 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("Activity Lifecycle", "onCreate invoked");
+        Toast.makeText(this, "Created", Toast.LENGTH_SHORT).show();
+        TextView tv = findViewById(R.id.txt);
+        tv.setText("Created");
     }
+
+    @Override
+    protected void onStart() {
+
+        super.onStart();
+        Log.d("Activity Lifecycle", "onStarted");
+        Toast.makeText(this, "Started", Toast.LENGTH_SHORT).show();
+        TextView tv = findViewById(R.id.txt);
+        tv.setText("Started");
+    }
+
+    @Override
+    protected void onResume() {
+
+        super.onResume();
+        Log.d("Activity Lifecycle", "onResume invoked");
+        Toast.makeText(this, "Resumed", Toast.LENGTH_SHORT).show();
+        TextView tv = findViewById(R.id.txt);
+        tv.setText("Resumed");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("Activity Lifecycle", "onPause invoked");
+        Toast.makeText(this, "Paused", Toast.LENGTH_SHORT).show();
+        TextView tv = findViewById(R.id.txt);
+        tv.setText("Paused");
+    }
+
+    @Override
+    protected void onStop() {
+
+        super.onStop();
+        Log.d("Activity Lifecycle", "onStop invoked");
+        Toast.makeText(this, "Stopped", Toast.LENGTH_SHORT).show();
+        TextView tv = findViewById(R.id.txt);
+        tv.setText("Stopped");
+    }
+
+    @Override
+    protected void onRestart() {
+
+        super.onRestart();
+        Log.d("Activity Lifecycle", "onRestart invoked");
+        Toast.makeText(this, "Restarted", Toast.LENGTH_SHORT).show();
+        TextView tv = findViewById(R.id.txt);
+        tv.setText("Restarted");
+    }
+
+    @Override
+    protected void onDestroy() {
+
+        super.onDestroy();
+        Log.d("Activity Lifecycle", "onDestroy invoked");
+        Toast.makeText(this, "Destroyed", Toast.LENGTH_SHORT).show();
+        TextView tv = findViewById(R.id.txt);
+        tv.setText("Destroyed");
+    }
+
+
+
 }
