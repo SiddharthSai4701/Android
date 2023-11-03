144109c1b9e4a9ab52eddb40087edf062e4a9b18
Siddharth Vijay Sai <67595252+SiddharthSai4701@users.noreply.github.com>
2023-11-03 23:07:00 +0530

    UI designed. Username and password fields added. Password matching added. Further constraints required

--- 1_LoginPage/app/src/main/java/com/example/loginpage/MainActivity.java
+++ 1_LoginPage/app/src/main/java/com/example/loginpage/MainActivity.java
@@ -3,12 +3,41 @@ package com.example.loginpage;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    String UN = "sidvsai";
+    String PW = "SidVSai@123";
+    int checkTrials = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void check(View view) {
+        TextView un = findViewById(R.id.userName);
+        String username = un.getText().toString();
+
+        TextView pw = findViewById(R.id.password);
+        String password = pw.getText().toString();
+
+        if (password.equals(PW) && checkTrials<=3) {
+            Toast.makeText(this, "Correct password!!", Toast.LENGTH_SHORT).show();
+        }
+
+        else if(checkTrials>3) {
+            Toast.makeText(this, "Too many failed attempts!!", Toast.LENGTH_SHORT).show();
+        }
+
+        else {
+            Toast.makeText(this, "Password incorrect! Attempts: "+Integer.toString(checkTrials), Toast.LENGTH_SHORT).show();
+            checkTrials+=1;
+        }
     }
 }
