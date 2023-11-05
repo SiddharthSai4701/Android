a51dfac5e707353b324004b4b1bceecbafb943ef
Siddharth Vijay Sai <67595252+SiddharthSai4701@users.noreply.github.com>
2023-11-05 16:55:28 +0530

    Testing required for back button

--- 4_Intents/app/src/main/java/com/example/intents/MainActivity.java
+++ 4_Intents/app/src/main/java/com/example/intents/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.intents;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button implicit_btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        implicit_btn = findViewById(R.id.implicitIntent);
+
+        implicit_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://milestone-monitor.vercel.app/"));
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void explicitClicked(View view) {
+        Intent intent = new Intent(getBaseContext(), MainActivity2.class);
+        startActivity(intent);
     }
 }
---
+++ 4_Intents/app/src/main/java/com/example/intents/MainActivity2.java
@@ -0,0 +1,31 @@
+package com.example.intents;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    Button back;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main2);
+
+
+        Toast.makeText(getApplicationContext(), "We are moved to second Activity",Toast.LENGTH_LONG).show();
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getBaseContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
