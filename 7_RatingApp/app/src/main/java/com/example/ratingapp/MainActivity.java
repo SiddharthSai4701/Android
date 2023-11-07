17eca4384f9643d451093dc29d9afbaf89cf75ca
Siddharth Vijay Sai <67595252+SiddharthSai4701@users.noreply.github.com>
2023-11-08 00:07:54 +0530

    Update MainActivity.java

--- 7_RatingApp/app/src/main/java/com/example/ratingapp/MainActivity.java
+++ 7_RatingApp/app/src/main/java/com/example/ratingapp/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.ratingapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RatingBar;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    RatingBar ratingBar;
+    Button submit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ratingBar = findViewById(R.id.ratingBar);
+        submit = findViewById(R.id.submit);
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String rating = String.valueOf(ratingBar.getRating());
+                Toast.makeText(MainActivity.this, ""+rating, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
