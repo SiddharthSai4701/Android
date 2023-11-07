be9db20033b8fe367bb57a49faba9b62858d3b77
Siddharth Vijay Sai <67595252+SiddharthSai4701@users.noreply.github.com>
2023-11-07 22:37:33 +0530

    Update MainActivity.java

--- 6_AudioVideoApp/app/src/main/java/com/example/audiovideoplayer/MainActivity.java
+++ 6_AudioVideoApp/app/src/main/java/com/example/audiovideoplayer/MainActivity.java
@@ -2,13 +2,60 @@ package com.example.audiovideoplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.MediaParser;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    MediaPlayer mediaPlayer;
+    Button btn_play, btn_pause, btn_stop, nextIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.trial);
+
+        btn_play = findViewById(R.id.play);
+        btn_pause = findViewById(R.id.pause);
+        btn_stop = findViewById(R.id.stop);
+        nextIntent = findViewById(R.id.nextPage);
+
+        btn_play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayer.start();
+            }
+        });
+
+        btn_pause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayer.pause();
+            }
+        });
+
+        btn_stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayer.stop();
+            }
+        });
+
+        nextIntent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getBaseContext(), MainActivity2.class);
+                startActivity(intent);
+            }
+        });
+
+
+
     }
 }
